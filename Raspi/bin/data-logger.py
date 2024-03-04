641eb4dd729ae2b9f7e9d6c8b34f1f722f8a7dff
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-03-04 15:27:18 +0100

    Raspi/: MQTT source

--- Raspi/bin/data-logger.py
+++ Raspi/bin/data-logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from endless.source_can import CANSource
+from endless.source_mqtt import MQTTSource
 from endless.source_mock import MockSource
 from endless.sink_stdout import StdoutSink
 from endless import async_util
@@ -11,7 +12,8 @@ import asyncio
 sources = [
     CANSource(name='CAN#42', can_iface='mein-test-can', can_id=42),
     CANSource(name='CAN#01', can_iface='mein-test-can', can_id=1),
-    MockSource(name='MOCK', timestamps = async_util.wall_timestamps(400, 400), temperature=37.5)
+    MockSource(name='MOCK', timestamps = async_util.wall_timestamps(400, 400), temperature=37.5),
+    MQTTSource(name='MQTT.egon', host='localhost', topic='egon'),
 ]
 
 sink = StdoutSink()
---
+++ Raspi/endless/source_mqtt.py
@@ -0,0 +1,28 @@
+from .source import Source
+from .sample import Sample
+
+import json
+import aiomqtt
+
+
+class MQTTSource(Source):
+    def __init__(self, name, host, topic, port = 1883):
+        super().__init__(name)
+
+        self.host = host
+        self.port = port
+        self.topic = topic
+
+    async def _run(self):
+        async with aiomqtt.Client(hostname=self.host, port=self.port) as client:
+            await client.subscribe(self.topic)
+            async for message in client.messages:
+                mqtt_sample = json.loads(message.payload)
+                await self.sink.put(
+                    Sample(
+                        name = self.name,
+                        timestamp_ms = mqtt_sample['timestamp_ms'],
+                        temperature = mqtt_sample['temperature'],
+                    ),
+                )
+                
