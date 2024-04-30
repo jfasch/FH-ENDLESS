1fd546fde032ae8128393a66a1abb49cd9c04827
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-04-30 14:11:46 +0200

    Raspi/: Sample has tag not name

--- Raspi/endless/sink_stdout.py
+++ Raspi/endless/sink_stdout.py
@@ -6,5 +6,5 @@ from .interfaces import Inlet
 @facet('inlet', Inlet, (('consume_sample', '_handle_put'),))
 class StdoutSink(Component):
     async def _handle_put(self, sample):
-        print(f'{sample.name};{sample.timestamp};{sample.data}')
+        print(f'{sample.tag};{sample.timestamp};{sample.data}')
         
