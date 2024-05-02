59cd3229f9bcc8222af3e75f5720bf6e95b50550
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-05-02 12:18:18 +0200

    Raspi/: SampleBroadcaster in-terms-of multi-receptacle

--- Raspi/endless/sample_broadcaster.py
+++ Raspi/endless/sample_broadcaster.py
@@ -1,25 +1,11 @@
 from .component import Component
 from .facet import facet
+from .receptacle import receptacle, ONE_OR_MANY
 from .interfaces import SampleInlet
 
 
 @facet('sample_in', SampleInlet, (('consume_sample', '_put_sample'),))
+@receptacle('sample_out', SampleInlet, multiplicity=ONE_OR_MANY)
 class SampleBroadcaster(Component):
-    def __init__(self):
-        super().__init__()
-        self._sample_out = self._multi_outlet()
-        self.sample_out = self._sample_out
-
     async def _put_sample(self, sample):
         await self._sample_out.consume_sample(sample)
-
-    class _multi_outlet:
-        def __init__(self):
-            self.targets = []
-        def connect(self, target):
-            assert isinstance(target, SampleInlet)
-            self.targets.append(target)
-        async def consume_sample(self, sample):
-            for target in self.targets:
-                retval = await target.consume_sample(sample)
-                assert retval is None
