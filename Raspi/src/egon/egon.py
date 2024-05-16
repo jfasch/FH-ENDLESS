4d4b4f1455d6f402d21f505d73bcfa6e7365d74c
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-05-16 13:43:07 +0200

    Raspi/: split can_switch out of egon

--- Raspi/src/egon/egon.py
+++ Raspi/src/egon/egon.py
@@ -1,9 +1,8 @@
 from endless.component import Component
 from endless.facet import facet
 from endless.receptacle import receptacle, ONE
-from endless.interfaces import CANInputHandler, CANOutputHandler, SampleInlet, Switch, Control, Counter
+from endless.interfaces import CANInputHandler, SampleInlet, Control
 from endless.sample import Sample
-from endless.can_util import CANFrame
 from endless.async_util import wallclock_timestamps_nosleep
 
 from dataclasses import dataclass
@@ -71,30 +70,3 @@ def transform_hum_temp_to_temp(sample):
                   timestamp=sample.timestamp,
                   data=sample.data.temperature,
                   )
-
-@facet('switch', Switch, (('set_state', '_set_state'),))
-@facet('counter', Counter, (('get_count', '_get_counter'),))
-@receptacle('frame_out', CANOutputHandler, multiplicity=ONE)
-class CANSwitch(Component):
-    DATA_LAYOUT = "????"      # 4 bytes/bools, 4 switches
-
-    def __init__(self, can_id, number):
-        super().__init__()
-        self.can_id = can_id
-        self.number = number
-        self.switch_counter = 0
-
-    async def _set_state(self, state):
-        states = [0,0,0,0]
-        states[self.number] = state
-
-        payload = struct.pack(self.DATA_LAYOUT, *states)
-
-        await self._frame_out.write_frame(
-            can_id=self.can_id, 
-            payload=struct.pack(self.DATA_LAYOUT, *states))
-
-        self.switch_counter += 1
-
-    async def _get_counter(self):
-        return self.switch_counter
