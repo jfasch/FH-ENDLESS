b44b4faf28068a20d31535cee681e1de2de32e2d
Joerg Faschingbauer <jf@faschingbauer.co.at>
2025-01-28 11:02:44 +0100

    Raspi: rename SampleConverter back to SampleFilter

--- Raspi/src/endless/framework/sample_filter.py
+++ Raspi/src/endless/framework/sample_filter.py
@@ -6,7 +6,7 @@ from .interfaces import SampleInlet
 
 @facet('sample_in', SampleInlet, (('consume_sample', '_convert'),))
 @receptacle('sample_out', SampleInlet, multiplicity=ONE)
-class SampleConverter(Component):
+class SampleFilter(Component):
     def __init__(self, func):
         super().__init__()
         self.func = func
